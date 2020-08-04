9118ea597e30296ba0124d2fc38382d48282a60f
rafaoctavio <rafaoctavio@gmail.com>
2020-08-04 16:56:55 -0300

    atualizando aulas

---
+++ exercicios-web/fundamentos/nullUndefined.js
@@ -0,0 +1,3 @@
+let valor // nao inicializada
+console.log(valor);
+
--- exercicios-web/fundamentos/objeto.js
+++ exercicios-web/fundamentos/objeto.js
@@ -1,2 +1,15 @@
 //Anotação literal de Objetos
 
+const prod1 = {};
+prod1.nome = "Iphone XS"
+prod1.preco = 6000.00
+prod1['Desconto Legal'] = 0.40 //evitar atributos com espaços
+
+console.log(prod1);
+
+const prod2 = {
+    nome:'Camisa Polo',
+    preco: 79.90,
+}
+
+console.log(prod2);
